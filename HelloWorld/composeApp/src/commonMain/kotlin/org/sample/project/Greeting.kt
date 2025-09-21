f1e1871217aced5e7abe8b59037f3509b9620a20
Devrath <devrath.dev595@gmail.com>
2025-09-21 21:44:35 +0530

    hello world

---
+++ HelloWorld/composeApp/src/commonMain/kotlin/org/sample/project/Greeting.kt
@@ -0,0 +1,9 @@
+package org.sample.project
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ HelloWorld/composeApp/src/jvmMain/kotlin/org/sample/project/main.kt
@@ -0,0 +1,13 @@
+package org.sample.project
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Demo",
+    ) {
+        App()
+    }
+}
